fc31268d1cfad588c4e6c559b811f3d08877edad
Igor Souza <igorsouza.dev@gmail.com>
2020-03-20 19:40:23 -0300

    Added spot creation with thumbnail upload

--- src/app/controllers/SpotsController.ts
+++ src/app/controllers/SpotsController.ts
@@ -7,7 +7,15 @@ export default {
     return res.json(spots);
   },
   async store(req: Request, res: Response) {
-    const spot = await Spot.create({ ...req.body, user_id: req.userId });
+    const { filename } = req.file;
+    const { company, price, techs } = req.body;
+    const spot = await Spot.create({
+      thumbnail: filename,
+      company,
+      price,
+      techs,
+      user_id: req.userId,
+    });
     return res.json(spot);
   },
 };
--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import authMiddleware from './app/middlewares/auth';
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 import SpotsController from './app/controllers/SpotsController';
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.post('/signup', UserController.store);
 routes.post('/signin', SessionController.store);
 
 routes.use(authMiddleware);
 routes.get('/spots', SpotsController.index);
-routes.post('/spots', SpotsController.store);
+routes.post('/spots', upload.single('thumbnail'), SpotsController.store);
 
 export default routes;
